0bb10c383f1066e509341ddeb2af952a11447d8e
OriB <ori.berrebi@runi.ac.il>
2024-01-07 23:25:49 +0200

    HW3

--- Calendar.java
+++ Calendar.java
@@ -23,7 +23,7 @@ public class Calendar {
 	    int debugDaysCounter = 0; 
 	    //// Write the necessary initialization code, and replace the condition
 	    //// of the while loop with the necessary condition 
-		while (dayOfMonth != 31 || month != 12 || year != (yearInput)) {
+		while (dayOfMonth != 31 || month != 12 || year != 1999) {
 			advance();
 	 		debugDaysCounter++;
 
